0d42bbbd67acd3b0670ba86ab55c3cfc45ab5eb8
amit <amitfr2000@gmail.com>
2022-07-30 03:33:42 +0300

    The file loading window now works
    You can search for files and your text is shown in the screen

--- include/EmuFileLoadingWindow.hpp
+++ include/EmuFileLoadingWindow.hpp
@@ -5,8 +5,9 @@
 
 #include "EmuBaseWindow.hpp"
 #include "EmuTextInputHelper.hpp"
+#include "EmuTextOutputHelper.hpp"
 
-class EmuFileLoadingWindow : public EmuBaseWindow, public EmuTextInputHelper {
+class EmuFileLoadingWindow : public EmuBaseWindow, public EmuTextInputHelper, public EmuTextOutputHelper {
 public:
     using TransferFile = std::function<void (std::fstream && file)>;
     EmuFileLoadingWindow(std::string && window_title, EmuBaseWindow::WindowPos && window_pos, uint32_t window_flags, TransferFile transfer_function);
@@ -17,5 +18,4 @@ private:
     void LineChecker() override;
 
     TransferFile _transfer_function;
-    std::string _line;
 };
--- src/EmuFileLoadingWindow.cpp
+++ src/EmuFileLoadingWindow.cpp
@@ -1,17 +1,32 @@
-#include <iostream>
+#include <SDL2/SDL_ttf.h>
+
 #include "../include/EmuFileLoadingWindow.hpp"
+#include "EmuSdlColorHelper.hpp"
 
 EmuFileLoadingWindow::EmuFileLoadingWindow(std::string && window_title, EmuBaseWindow::WindowPos && window_pos, uint32_t window_flags, TransferFile transfer_function) :  
-    EmuBaseWindow(std::move(window_title), std::move(window_pos), std::move(window_flags)),
+    EmuBaseWindow(std::move("File_Load_Window"), std::move(window_pos), std::move(window_flags), COLOR_RED),
     _transfer_function(transfer_function)
 {
-    _event_mapper.insert(std::make_pair(SDL_EventType::SDL_TEXTINPUT, std::bind(&EmuFileLoadingWindow::TextHandler, this, std::placeholders::_1)));
-    _event_mapper.insert(std::make_pair(SDL_EventType::SDL_KEYDOWN, std::bind(&EmuFileLoadingWindow::TextHandler, this, std::placeholders::_1)));
+    EmuTextInputHelper::InterfaceSetup(_event_mapper);
+    EmuTextOutputStyle s;
+    s.font_path = "../fonts/PressStart2P-Regular.ttf";
+    s.font_size = 24;
+    s.text_color = COLOR_WHITE;
+    EmuTextOutputHelper::InterfaceSetup(std::move(s));
 }
 
 void EmuFileLoadingWindow::EmuRenderWindow()
 {
-    // TODO: render window
+    SDL_RenderClear(_renderer.get());
+
+    std::shared_ptr<SDL_Texture> texture(std::move(CreateTextTexture(_line, _renderer)));
+
+    SDL_Rect Message_rect = EmuTextOutputHelper::GetTrueTextRectangleDim(_line);
+    Message_rect.x = (_window_pos.dim.width < Message_rect.w)? _window_pos.dim.width - Message_rect.w : 0;
+    Message_rect.y = 0;
+
+    SDL_RenderCopy(_renderer.get(), texture.get(), NULL, &Message_rect);
+    SDL_RenderPresent(_renderer.get());
 }
 
 // LineChecker This function checks the line so we will be able to load a file from the fs
@@ -22,6 +37,7 @@ void EmuFileLoadingWindow::LineChecker()
     if (fs.is_open())
     {
         _transfer_function(std::move(fs));
+        flag = true;
     }
     SetAfterEnterMessage(std::move(((flag)? "SUCCESS" : "FAIL")));
 }
