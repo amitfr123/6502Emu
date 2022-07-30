9894f2daeab65b50bd75fd2bf2de5f192dbcb8ab
amit <amitfr2000@gmail.com>
2022-07-30 03:34:47 +0300

    Main now runs a cute little example of the file load window

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
+#include <memory.h>
+
+#include "EmuWindowManager.hpp"
+#include "EmuFileLoadingWindow.hpp"
+
+static void func(std::fstream && file)
+{
+    std::cout << "cool" << std::endl;
+    file.close();
+}
 
 int main()
 {
-    std::cout << "hello world" << std::endl;
+    EmuWindowManager m;
+    std::string t = "wow";
+    EmuBaseWindow::WindowPos pos;
+    pos.dim.hight = 24;
+    pos.dim.width = 500;
+    pos.point.x = 0;
+    pos.point.y = 0;
+    std::shared_ptr<EmuFileLoadingWindow> win = std::make_shared<EmuFileLoadingWindow>(std::move(t), std::move(pos), SDL_WINDOW_OPENGL, std::bind(&func, std::placeholders::_1));
+    win->EmuInitWindow();
+    m.AddNewWindow(win);
+    m.EmuWindowManagerEventLoop();
     return 0;
 }
