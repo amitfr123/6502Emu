613749aeb73d83629de53c6a59eb1e40fa33b19d
amit <amitfr2000@gmail.com>
2022-07-30 15:22:19 +0300

    Cleaned up the output text helper

--- include/WindowUtilities/OutputTextHelper.hpp
+++ include/WindowUtilities/OutputTextHelper.hpp
@@ -9,43 +9,13 @@
 
 class OutputTextHelper {
 public:
-    OutputTextHelper(const std::string& font_path, const uint32_t font_size, SDL_Color text_color) 
-        :
-        _color(std::move(text_color))
-    {
-        _font.reset(TTF_OpenFont(font_path.c_str(), font_size), &TTF_CloseFont);
-        if (_font.get() == nullptr)
-        {
-            throw std::runtime_error("Invalid font path");
-        }
-    }
+    OutputTextHelper(const std::string& font_path, const uint32_t font_size, SDL_Color text_color);
 
-    std::shared_ptr<SDL_Texture> CreateTextTexture(const std::string& text, std::shared_ptr<SDL_Renderer> renderer)
-    {
-        if (_font.get() == nullptr)
-        {
-            return nullptr;
-        }
-        std::shared_ptr<SDL_Surface> surface(TTF_RenderText_Solid(_font.get(), text.c_str(), _color), SDL_FreeSurface);
-        if (surface.get() == nullptr)
-        {
-            return nullptr;
-        }
-        std::shared_ptr<SDL_Texture> texture(SDL_CreateTextureFromSurface(renderer.get(), surface.get()), SDL_DestroyTexture);
-        if (texture.get() == nullptr)
-        {
-            return nullptr;
-        }
-        return texture;
-    }
+    std::shared_ptr<SDL_Texture> CreateTextTexture(const std::string& text, std::shared_ptr<SDL_Renderer> renderer);
 
-    SDL_Rect GetTrueTextRectangleDim(const std::string& text)
-    {
-        SDL_Rect dim_rect;
-        TTF_SizeText(_font.get(), text.c_str(), &dim_rect.w, &dim_rect.h);
-        return dim_rect;
-    }
+    SDL_Rect GetTrueTextRectangleDim(const std::string& text);
 
+    ~OutputTextHelper() = default;
 private:
     std::shared_ptr<TTF_Font> _font;
     SDL_Color _color;
